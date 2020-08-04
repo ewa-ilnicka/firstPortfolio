09b5de450c756177d6b1f167119df9da663410fd
unknown <ewa.h.ilnicka@gmail.com>
2020-08-04 22:00:10 +0200

    Translated errors to polish.

--- contact.php
+++ contact.php
@@ -7,10 +7,10 @@ if (isset($_POST['Email'])) {
 
     function problem($error)
     {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
+        echo "Niestety formularz wydaje się zawierać błąd. ";
+        echo "Poniżej znajdują się następujące błędy:<br><br>";
         echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
+        echo "Proszę naciśnij 'Wstecz' i popraw wymienione błędy.<br><br>";
         die();
     }
 
@@ -31,17 +31,17 @@ if (isset($_POST['Email'])) {
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
     if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+        $error_message .= 'Wprowadź proszę poprawny adres email.<br>';
     }
 
     $string_exp = "/^[A-Za-z .'-ąĄęĘżŻźŹóÓśŚćĆłŁ]+$/";
 
     if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+        $error_message .= 'Wprowadź proszę poprawne imię oraz nazwisko.<br>';
     }
 
     if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+        $error_message .= 'Wprowadź proszę poprawną wiadomość.<br>';
     }
 
     if (strlen($error_message) > 0) {
@@ -69,7 +69,7 @@ if (isset($_POST['Email'])) {
 
     <!-- include your success message below -->
 
-    Thank you for contacting us. We will be in touch with you very soon.
+    Dziękuję za wiadomość. W miarę możliwości postaram się szybko odpowiedzieć.
 
 <?php
 }
